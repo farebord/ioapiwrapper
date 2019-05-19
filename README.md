431ba5f8df0178f8542bf61132343b04d3a6a393
Facundo Rebord <farebord@gmail.com>
2019-05-19 19:44:39 -0300

    Added missing API wrapper functions, change README.md

--- README.md
+++ README.md
@@ -5,15 +5,4 @@ InvertirOnline API wrapper built with Python3.
 
 - Add a request class that handle all the status/response of the request.
 - Add missing unit tests.
-- Add missing API request to the wrapper:
-    - "POST /api/v2/operar/Vender",
-    - "POST /api/v2/operar/Comprar",
-    - "POST /api/v2/operar/rescate/fci",
-    - "POST /api/v2/operar/suscripcion/fci",
-    - "GET /api/v2/Cotizaciones/{Instrumento}/{Panel}/{Pais}",
-    - "GET /api/v2/{pais}/Titulos/Cotizacion/Paneles/{instrumento}",
-    - "GET /api/v2/{Mercado}/Titulos/{Simbolo}/Cotizacion",
-    - "GET /api/v2/Titulos/FCI/Administradoras/{administradora}/TipoFondos",
-    - "GET /api/v2/Titulos/FCI/Administradoras/{administradora}/TipoFondos/{tipoFondo}",
-    - "GET /api/v2/{mercado}/Titulos/{simbolo}/Cotizacion/seriehistorica/{fechaDesde}/{fechaHasta}/{ajustada}"
-
+- Better function naming.
--- ioapiwrapper/wrapper.py
+++ ioapiwrapper/wrapper.py
@@ -9,12 +9,21 @@ class IOBase:
     URL_MUTUAL_FUND_OPTIONS = "/api/v2/{market}/Titulos/{symbol}/Opciones"
     URL_MUTUAL_FUNDS = "/api/v2/Titulos/FCI"
     URL_MUTUAL_FUNDS_ADMINS = "/api/v2/Titulos/FCI/Administradoras"
+    URL_MUTUAL_FUNDS_BY_ADMIN_AND_TYPE = "/api/v2/Titulos/FCI/Administradoras/{admin}/TipoFondos/{fcitype}"
     URL_MUTUAL_FUNDS_TYPES = "/api/v2/Titulos/FCI/TipoFondos"
+    URL_MUTUAL_FUNDS_TYPES_BY_ADMIN = "/api/v2/Titulos/FCI/Administradoras/{admin}/TipoFondos"
+    URL_INSTRUMENT = "/api/v2/{country}/Titulos/Cotizacion/Paneles/{instrument}"
     URL_INSTRUMENTS = "/api/v2/{country}/Titulos/Cotizacion/Instrumentos"
+    URL_OPERATE_BUY = "/api/v2/operar/Comprar"
+    URL_OPERATE_SELL = "/api/v2/operar/Vender"
+    URL_OPERATE_SUBSCRIBE = "/api/v2/operar/suscripcion/fci"
+    URL_OPERATE_RESCUE = "/api/v2/operar/rescate/fci"
     URL_OPERATION = "/api/v2/operaciones/{number}"
     URL_OPERATIONS = "/api/v2/operaciones/"
     URL_OPERATIONS_DELETE = "/api/v2/operaciones/{number}"
     URL_PORTFOLIO = "/api/v2/portafolio/{country}"
+    URL_STOCK = "/api/v2/{market}/Titulos/{symbol}/Cotizacion"
+    URL_STOCK_HISTORY = "/api/v2/{market}/Titulos/{symbol}/Cotizacion/seriehistorica/{date_from}/{date_to}/{fit}"
     URL_TOKEN = "/token"
 
 
@@ -33,8 +42,20 @@ class IOWrapper(IOBase):
         self.access_token = response.get('access_token')
         self.refresh_token = response.get('refresh_token')
         self.token_issued = response.get('.issued')
-        self.token_expires = response.get('.expires') 
-    
+        self.token_expires = response.get('.expires')
+
+    def buy(self, market, symbol, amount, price, valid_date, term):
+        payload = {
+            "mercado": market,
+            "simbolo": symbol,
+            "cantidad": amount,
+            "precio": price,
+            "validez": valid_date,
+            "plazo": term
+        }
+        request = requests.post(self.api + self.URL_OPERATE_BUY, json=payload, headers=self._get_bearer_header())
+        return request.json()
+
     def delete_operation(self, number):
         request = requests.delete(self.api + self.URL_OPERATIONS_DELETE.format(number),
                                   headers=self._get_bearer_header())
@@ -42,6 +63,11 @@ class IOWrapper(IOBase):
 
     def get_account_state(self):
         request = requests.get(self.api + self.URL_ACCOUNT_STATE, headers=self._get_bearer_header())
+        return request.json()
+
+    def get_instrument(self, country, instrument):
+        request = requests.get(self.api + self.URL_INSTRUMENT.format(country=country, instrument=instrument),
+                               headers=self._get_bearer_header())
         return request.json()
 
     def get_instruments(self, country):
@@ -70,6 +96,10 @@ class IOWrapper(IOBase):
                                headers=self._get_bearer_header())
         return request.json()
 
+    def get_mutual_fund_types(self):
+        request = requests.get(self.api + self.URL_MUTUAL_FUNDS_TYPES, headers=self._get_bearer_header())
+        return request
+
     def get_mutual_funds(self):
         request = requests.get(self.api + self.URL_MUTUAL_FUNDS, headers=self._get_bearer_header())
         return request.json()
@@ -78,9 +108,15 @@ class IOWrapper(IOBase):
         request = requests.get(self.api + self.URL_MUTUAL_FUNDS_ADMINS, headers=self._get_bearer_header())
         return request.json()
 
-    def get_mutual_fund_types(self):
-        request = requests.get(self.api + self.URL_MUTUAL_FUNDS_TYPES, headers=self._get_bearer_header())
-        return request
+    def get_mutual_fund_types_by_admin(self, admin):
+        request = requests.get(self.api + self.URL_MUTUAL_FUNDS_TYPES_BY_ADMIN.format(admin=admin),
+                               headers=self._get_bearer_header())
+        return request.json()
+
+    def get_mutual_fund_by_admin_and_type(self, admin, fcitype):
+        request = requests.get(self.api + self.URL_MUTUAL_FUNDS_BY_ADMIN_AND_TYPE.format(admin=admin, fcitype=fcitype),
+                               headers=self._get_bearer_header())
+        return request.json()
 
     def get_portfolio(self, country):
         request = requests.get(self.api + self.URL_PORTFOLIO.format(country=country), headers=self._get_bearer_header())
@@ -94,6 +130,17 @@ class IOWrapper(IOBase):
         request = requests.get(self.api + self.URL_OPERATIONS, headers=self._get_bearer_header())
         return request.json()
 
+    def get_stock(self, market, symbol):
+        request = requests.get(self.api + self.URL_STOCK.format(market=market, symbol=symbol),
+                               headers=self._get_bearer_header())
+        return request.json()
+
+    def get_stock_history(self, market, symbol, date_from, date_to, fit):
+        request = requests.get(self.api + self.URL_STOCK_HISTORY.format(market=market, symbol=symbol,
+                                                                        date_from=date_from, date_to=date_to, fit=fit),
+                               headers=self._get_bearer_header())
+        return request.json()
+
     def get_token(self, username=None, password=None, refresh_token=None, grant_type="password"):
         payload = {}
         if grant_type == "password":
@@ -101,9 +148,39 @@ class IOWrapper(IOBase):
         elif grant_type == "refresh_token":
             payload = {"refresh_token": refresh_token, "grant_type": grant_type}
         
-        response = requests.post(self.api + self.URL_TOKEN, data=payload)
+        response = requests.post(self.api + self.URL_TOKEN, json=payload)
         self._store_token_info(response.json())
         return response.json()
+
+    def rescue(self, symbol, amount, validate=None):
+        payload = {
+            "simbolo": symbol,
+            "cantidad": amount,
+            "soloValidar": validate
+        }
+        request = requests.post(self.api + self.URL_OPERATE_RESCUE, json=payload, headers=self._get_bearer_header())
+        return request.json()
+
+    def sell(self, market, symbol, amount, price, valid_date, term=None):
+        payload = {
+            "mercado": market,
+            "simbolo": symbol,
+            "cantidad": amount,
+            "precio": price,
+            "validez": valid_date,
+            "plazo": term
+        }
+        request = requests.post(self.api + self.URL_OPERATE_SELL, json=payload, headers=self._get_bearer_header())
+        return request.json()
+
+    def subscribe(self, symbol, amount, validate):
+        payload = {
+            "simbolo": symbol,
+            "cantidad": amount,
+            "soloValidar": validate
+        }
+        request = requests.post(self.api + self.URL_OPERATE_SUBSCRIBE, json=payload, headers=self._get_bearer_header())
+        return request.json()
 
 
 class IOService(IOWrapper):
